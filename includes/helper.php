be07ab655dba4f04a5c095202d2b75eb74ccdf5d
zrvincent <pqkhanhgl@gmail.com>
2025-03-17 10:41:07 +0700

    refactor(acf-icomoon): check file is not JSON file

--- includes/helper.php
+++ includes/helper.php
@@ -22,6 +22,10 @@ if(!function_exists('viivue_array_key_exists')){
 if(!function_exists('viivue_get_icomoon_json_path')){
 	function viivue_get_icomoon_json_path($json_path = ''){
 		if(!empty($json_path)){
+			$file_name = basename($json_path);
+			if(pathinfo($file_name, PATHINFO_EXTENSION) !== 'json'){
+				return '';
+			}
 			$theme_path      = get_stylesheet_directory();
 			$json_path_array = array_unique(array_merge(explode('/', $theme_path), explode('/', $json_path)));
 			$json_path       = implode('/', $json_path_array);
