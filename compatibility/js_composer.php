64d68bad3ef65c9ce4a16add12255f8154075733
daomapsieucap <bichdao.chau@gmail.com>
2025-06-02 17:22:31 +0700

    fix(js_composer): #28 fix wrong json path

--- compatibility/js_composer.php
+++ compatibility/js_composer.php
@@ -7,7 +7,7 @@ vc_add_shortcode_param('icomoon_class', 'viivue_js_composer_icomoon');
 function viivue_js_composer_icomoon($settings, $value){
 	$acf_icomoon = new ViiVue_ACF_Field_Icomoon();
 	
-	$json_path  = file_get_contents(ACFICOMOON_STYLESHEET_DIR . "/assets/fonts/selection.json");
+	$json_path  = ACFICOMOON_STYLESHEET_DIR . "/assets/fonts/selection.json";
 	$icon_array = $acf_icomoon->viivue_get_icomoon_json($json_path);
 	$type       = viivue_array_key_exists("type", $settings);
 	$param_name = viivue_array_key_exists("param_name", $settings);
